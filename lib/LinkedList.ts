4c3126bd0a86418c5e69f70b4b10a78eae7f9429
Sandro E. Martínez <zanddro@gmail.com>
2023-08-26 15:29:26 -0600

    basis

---
+++ lib/LinkedList.ts
@@ -0,0 +1,28 @@
+import { Node } from './Node';
+
+export class LinkedList<T> {
+  head: Node<T> | null = null;
+
+  append(value: T): void {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  print(): void {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
